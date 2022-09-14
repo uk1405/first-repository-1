e66077e3354a9c0b04570a8da625c38b93db2a23
kyungskim-kit <kyungskim.kit@gmail.com>
2022-09-14 19:03:51 +0900

    main and prime_number

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ if __name__ == '__main__':
     pn.is_prime_number(1)
     pn.is_prime_number(13)
     pn.is_prime_number(24)
-    pn.is_prime_number(31)
-    pn.is_prime_number(45)
-    pn.is_prime_number(97)
+
 
